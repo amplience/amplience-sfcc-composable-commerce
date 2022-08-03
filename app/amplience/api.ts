1b34582ca656152caa1c5c44fed3e5d4d54f5c56
Dave Lilly <dave@lillys.org>
2022-08-03 09:43:27 -0500

    api

---
+++ app/amplience/api.ts
@@ -0,0 +1,23 @@
+import { ContentClient } from 'dc-delivery-sdk-js';
+import { amplienceConfig } from '../../config/amplience.config'
+
+/*
+Make a service to set VSE on it...
+This should be where the VSE is checked generically.
+Get from URL first - Store in storage
+Get from storage
+Clear functionality would need to be there too.
+*/
+
+const client: ContentClient = new ContentClient({ hubName: amplienceConfig.hubName });
+export type IdOrKey = { id: string } | { key: string }
+async function fetchContent(args: IdOrKey[]) {
+    let responses = await (await client.getContentItems(args)).responses
+    return responses.map(response => {
+        if ('content' in response) {
+            return response.content
+        }
+        return response.error
+    })
+}
+export default fetchContent;
---
+++ config/amplience.config.js
@@ -0,0 +1,3 @@
+export const amplienceConfig = {
+    hub: 'aria'
+}
