a0d9c0007307a87bed1f71dde4d4097fc8131bbb
Dave Lilly <dave@lillys.org>
2022-08-03 10:13:56 -0500

    revert to old homepage

--- app/pages/home/index.jsx
+++ app/pages/home/index.jsx
@@ -7,15 +7,31 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useIntl } from 'react-intl'
+import {useIntl, FormattedMessage} from 'react-intl'
 
 // Components
 import {
-    Box} from '@chakra-ui/react'
+    Box,
+    Button,
+    SimpleGrid,
+    HStack,
+    VStack,
+    Text,
+    Flex,
+    Stack,
+    Container,
+    Link
+} from '@chakra-ui/react'
 
 // Project Components
+import Hero from '../../components/hero'
+import Seo from '../../components/seo'
+import Section from '../../components/section'
+import ProductScroller from '../../components/product-scroller'
 
 // Others
+import {getAssetUrl} from 'pwa-kit-react-sdk/ssr/universal/utils'
+import {heroFeatures, features} from './data'
 
 // Constants
 import {
@@ -25,45 +41,274 @@ import {
 } from '../../constants'
 import fetchContent from '../../amplience/api'
 
-import { JSONTree } from 'react-json-tree'
-
 /**
  * This is the home page for Retail React App.
  * The page is created for demonstration purposes.
  * The page renders SEO metadata and a few promotion
  * categories and products, data is from local file.
  */
-const Home = ({ homepage, slots, productSearchResult, isLoading }) => {
+const Home = ({productSearchResult, isLoading}) => {
     const intl = useIntl()
 
-    return <Box layerStyle="page">
-        <div>
-            {homepage && <div><b>key: 'homepage'</b><JSONTree data={homepage} hideRoot={true} /></div>}
-        </div>
-        {slots.map(slot => (<div><b>slot: '{slot._meta.name}'</b><JSONTree data={slot} hideRoot={true} /></div>))}
-    </Box>
+    fetchContent('995ad4df-d340-4172-a3b6-dcf30694a2d2');
+
+    return (
+        <Box data-testid="home-page" layerStyle="page">
+            <Seo
+                title="Home Page"
+                description="Commerce Cloud Retail React App"
+                keywords="Commerce Cloud, Retail React App, React Storefront"
+            />
+
+            <Hero
+                title={intl.formatMessage({
+                    defaultMessage: 'XXX The React PWA Starter Store for Retail',
+                    id: 'home.title.react_starter_store'
+                })}
+                img={{
+                    src: getAssetUrl('static/img/hero.png'),
+                    alt: 'npx pwa-kit-create-app'
+                }}
+                actions={
+                    <Stack spacing={{base: 4, sm: 6}} direction={{base: 'column', sm: 'row'}}>
+                        <Button
+                            as={Link}
+                            href="https://developer.salesforce.com/docs/commerce/pwa-kit-managed-runtime/guide/getting-started.html"
+                            target="_blank"
+                            width={{base: 'full', md: 'inherit'}}
+                            paddingX={7}
+                            _hover={{textDecoration: 'none'}}
+                        >
+                            <FormattedMessage
+                                defaultMessage="XX Get started"
+                                id="home.link.get_started"
+                            />
+                        </Button>
+                        <Button
+                            as={Link}
+                            href="https://developer.salesforce.com/docs/commerce/pwa-kit-managed-runtime/guide/getting-started.html"
+                            target="_blank"
+                            width={{base: 'full', md: 'inherit'}}
+                            paddingX={7}
+                            _hover={{textDecoration: 'none'}}
+                        >
+                            <FormattedMessage
+                                defaultMessage="XX Get started"
+                                id="home.link.get_started"
+                            />
+                        </Button>
+                    </Stack>
+                }
+            />
+
+            <Section
+                background={'gray.50'}
+                marginX="auto"
+                paddingY={{base: 8, md: 16}}
+                paddingX={{base: 4, md: 8}}
+                borderRadius="base"
+                width={{base: '100vw', md: 'inherit'}}
+                position={{base: 'relative', md: 'inherit'}}
+                left={{base: '50%', md: 'inherit'}}
+                right={{base: '50%', md: 'inherit'}}
+                marginLeft={{base: '-50vw', md: 'auto'}}
+                marginRight={{base: '-50vw', md: 'auto'}}
+            >
+                <SimpleGrid
+                    columns={{base: 1, md: 1, lg: 3}}
+                    spacingX={{base: 1, md: 4}}
+                    spacingY={{base: 4, md: 14}}
+                >
+                    {heroFeatures.map((feature, index) => {
+                        const featureMessage = feature.message
+                        return (
+                            <Box
+                                key={index}
+                                background={'white'}
+                                boxShadow={'0px 2px 2px rgba(0, 0, 0, 0.1)'}
+                                borderRadius={'4px'}
+                            >
+                                <Link target="_blank" href={feature.href}>
+                                    <HStack>
+                                        <Flex
+                                            paddingLeft={6}
+                                            height={24}
+                                            align={'center'}
+                                            justify={'center'}
+                                        >
+                                            {feature.icon}
+                                        </Flex>
+                                        <Text fontWeight="700">
+                                            {intl.formatMessage(featureMessage.title)}
+                                        </Text>
+                                    </HStack>
+                                </Link>
+                            </Box>
+                        )
+                    })}
+                </SimpleGrid>
+            </Section>
+
+            {productSearchResult && (
+                <Section
+                    padding={4}
+                    paddingTop={16}
+                    title={intl.formatMessage({
+                        defaultMessage: 'Shop Products',
+                        id: 'home.heading.shop_products'
+                    })}
+                    subtitle={intl.formatMessage(
+                        {
+                            defaultMessage:
+                                'This section contains content from the catalog. {docLink} on how to replace it.',
+                            id: 'home.description.shop_products',
+                            description:
+                                '{docLink} is a html button that links the user to https://sfdc.co/business-manager-manage-catalogs'
+                        },
+                        {
+                            docLink: (
+                                <Link
+                                    target="_blank"
+                                    href={'https://sfdc.co/business-manager-manage-catalogs'}
+                                    textDecoration={'none'}
+                                    position={'relative'}
+                                    _after={{
+                                        position: 'absolute',
+                                        content: `""`,
+                                        height: '2px',
+                                        bottom: '-2px',
+                                        margin: '0 auto',
+                                        left: 0,
+                                        right: 0,
+                                        background: 'gray.700'
+                                    }}
+                                    _hover={{textDecoration: 'none'}}
+                                >
+                                    {intl.formatMessage({
+                                        defaultMessage: 'Read docs',
+                                        id: 'home.link.read_docs'
+                                    })}
+                                </Link>
+                            )
+                        }
+                    )}
+                >
+                    <Stack pt={8} spacing={16}>
+                        <ProductScroller
+                            products={productSearchResult?.hits}
+                            isLoading={isLoading}
+                        />
+                    </Stack>
+                </Section>
+            )}
+
+            <Section
+                padding={4}
+                paddingTop={32}
+                title={intl.formatMessage({
+                    defaultMessage: 'Features',
+                    id: 'home.heading.features'
+                })}
+                subtitle={intl.formatMessage({
+                    defaultMessage:
+                        'Out-of-the-box features so that you focus only on adding enhancements.',
+                    id: 'home.description.features'
+                })}
+            >
+                <Container maxW={'6xl'} marginTop={10}>
+                    <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={10}>
+                        {features.map((feature, index) => {
+                            const featureMessage = feature.message
+                            return (
+                                <HStack key={index} align={'top'}>
+                                    <VStack align={'start'}>
+                                        <Flex
+                                            width={16}
+                                            height={16}
+                                            align={'center'}
+                                            justify={'left'}
+                                            color={'gray.900'}
+                                            paddingX={2}
+                                        >
+                                            {feature.icon}
+                                        </Flex>
+                                        <Text color={'black'} fontWeight={700} fontSize={20}>
+                                            {intl.formatMessage(featureMessage.title)}
+                                        </Text>
+                                        <Text color={'black'}>
+                                            {intl.formatMessage(featureMessage.text)}
+                                        </Text>
+                                    </VStack>
+                                </HStack>
+                            )
+                        })}
+                    </SimpleGrid>
+                </Container>
+            </Section>
+
+            <Section
+                padding={4}
+                paddingTop={32}
+                title={intl.formatMessage({
+                    defaultMessage: "We're here to help",
+                    id: 'home.heading.here_to_help'
+                })}
+                subtitle={
+                    <>
+                        <>
+                            {intl.formatMessage({
+                                defaultMessage: 'Contact our support staff.',
+                                id: 'home.description.here_to_help'
+                            })}
+                        </>
+                        <br />
+                        <>
+                            {intl.formatMessage({
+                                defaultMessage: 'They will get you to the right place.',
+                                id: 'home.description.here_to_help_line_2'
+                            })}
+                        </>
+                    </>
+                }
+                actions={
+                    <Button
+                        as={Link}
+                        href="https://help.salesforce.com/s/?language=en_US"
+                        target="_blank"
+                        width={'auto'}
+                        paddingX={7}
+                        _hover={{textDecoration: 'none'}}
+                    >
+                        <FormattedMessage defaultMessage="Contact Us" id="home.link.contact_us" />
+                    </Button>
+                }
+                maxWidth={'xl'}
+            />
+        </Box>
+    )
 }
 
 Home.getTemplateName = () => 'home'
 
-Home.shouldGetProps = ({ previousLocation, location }) =>
+Home.shouldGetProps = ({previousLocation, location}) =>
     !previousLocation || previousLocation.pathname !== location.pathname
 
-Home.getProps = async (props) => {
-    if (props.res) {
-        props.res.set('Cache-Control', `max-age=${MAX_CACHE_AGE}`)
+Home.getProps = async ({res, api}) => {
+    if (res) {
+        res.set('Cache-Control', `max-age=${MAX_CACHE_AGE}`)
     }
 
-    const productSearchResult = await props.api.shopperSearch.productSearch({
+    const productSearchResult = await api.shopperSearch.productSearch({
         parameters: {
             refine: [`cgid=${HOME_SHOP_PRODUCTS_CATEGORY_ID}`, 'htype=master'],
             limit: HOME_SHOP_PRODUCTS_LIMIT
         }
     })
 
-    const homepage = await (await fetchContent([{ key: 'homepage' }])).pop()
-    const slots = await (await fetchContent(homepage.slots.map(slot => ({ id: slot.id }))))
-    return { homepage, slots, productSearchResult }
+    // const homepage = await (await fetchContent([{ key: 'homepage' }])).pop()
+    // const slots = await (await fetchContent(homepage.slots.map(slot => ({ id: slot.id }))))
+
+    return {productSearchResult}
 }
 
 Home.propTypes = {
