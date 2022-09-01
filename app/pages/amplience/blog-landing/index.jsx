5f9c4e59a9812845703f572fee8bc826de66bc0d
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2022-09-01 13:05:16 +0100

    feat: Blog landing page - Chakra updates and algolia basics

--- app/pages/amplience/blog-landing/index.jsx
+++ app/pages/amplience/blog-landing/index.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // Algolia
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, RefinementList } from 'react-instantsearch-hooks-web';
 
 // Components
 import Button from '../../../components/amplience/button';
+import { SearchIcon } from '../../../components/icons';
 import {
     Box,
     Heading,
     SimpleGrid,
+    Input,
+    InputGroup,
+    InputLeftElement,
     VStack,
     Text,
-    Flex,
-    Stack,
-    Container,
-    Link,
-    Image
+    Image,
+    useMultiStyleConfig,
+    Select
 } from '@chakra-ui/react'
 
 
@@ -26,71 +27,183 @@ import {
  * The page renders SEO metadata and a few promotion
  * categories and products, data is from local file.
  */
-const BlogLanding = () => {
+const BlogLanding = (props) => {
 
-    const searchClient = algoliasearch('4BS5I6EVVD', 'fa9be8756d95a97f0021f0aba6f02bc4');
+    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState('');
+    const [authors, setAuthors] = useState([]);
+    const [author, setAuthor] = useState('');
+    const [tags, setTags] = useState([]);
+    const [tag, setTag] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState('');
 
-    function Hit({ hit }) {
-        console.log({hit});
+    const algoliaID = '4BS5I6EVVD'
+    const algoliaKey = 'fa9be8756d95a97f0021f0aba6f02bc4'
+    const algoliaIndexName = 'sfcccompdev.blog-staging'
+    const numItems = 8;
+
+    const searchClient = algoliasearch(algoliaID, algoliaKey);
+    const index = searchClient.initIndex(algoliaIndexName);
+
+    const facetHitsToArray = (list) => {
+        let facetArray = []
+        for (const property in list) {
+            facetArray.push({ value: property, count: list[property] })
+        }
+        return facetArray;
+
+    }
+
+    const fetchResults = () => {
+        // Lets get the facets
+        console.log("What is the query, ", query)
+        let filtersArray = []
+        if (tag) {
+            filtersArray.push('tags:' + tag)
+        }
+        if (author) {
+            filtersArray.push('author.name:' + author)
+        }
+        if (category) {
+            filtersArray.push('categories.name:' + category)
+        }
+        console.log(filtersArray)
+        index.search(query, {
+            facets: ['tags', 'author.name', 'categories.name'],
+            facetFilters: filtersArray,
+            hitsPerPage: numItems
+        }).then((results) => {
+            setTags(facetHitsToArray(results.facets.tags))
+            setCategories(facetHitsToArray(results.facets['categories.name']))
+            setAuthors(facetHitsToArray(results.facets['author.name']))
+            // Need to set the results of the renders
+            setResults(results.hits)
+
+            console.log("Combined results:", results);
+        });
+    }
+
+    useEffect(fetchResults, [query, tag, author, category]);
+
+    const onSearchInputChange = (e) => {
+        const input = e.target.value;
+        setQuery(input)
+    }
+
+    const onTagDropDownChanged = (e) => {
+        const input = e.target.value;
+        setTag(input)
+    }
+    const onAuthorDropDownChanged = (e) => {
+        const input = e.target.value;
+        setAuthor(input)
+    }
+    const onCategoryDropDownChanged = (e) => {
+        const input = e.target.value;
+        setCategory(input)
+    }
+
+
+
+    function BlogCard(props) {
+        const {
+            item,
+            key,
+            ...other
+        } = props;
+        const content = props.item
+        const styles = useMultiStyleConfig('ProductTile')
 
         /// the URL should be the delivery key
-        const linkurl = `/${hit.deliveryKey}`;
-        const imageurl = `${hit.image}&aspect-1:1&w=400`
+        const linkurl = `/${content.deliveryKey}`;
+        const imageurl = `${content.image}&aspect-1:1&w=400`
         return (
-            <Container>
-            <Box
-            background={'white'}
-            boxShadow={'0px 2px 2px rgba(0, 0, 0, 0.1)'}
-            borderRadius={'4px'}
-        >
-            <VStack>
-            {hit.name && (    
-                <Heading as="h1">{hit.name}</Heading>
-            )}
-            {hit.image && ( 
-            <Image
-                                fit={'cover'}
-                                align={'center'}
-                                width={'100%'}
-                                height={'100%'}
-                                src={imageurl}
-                                alt={hit.name}
-                            />
-             )}
-             {hit.description && ( 
-            <Text color={'gray.700'} fontWeight={600}>
-                {hit.description}
-            </Text>
-             )}
-             {hit.deliveryKey && ( 
-            <Box paddingTop="2" width={{base: 'full', md: 'auto'}}>
-                <Button label="View" url={linkurl} target='_self'></Button>
+            <Box {...styles.container}>
+
+
+                {content.name && (
+                    <Heading as="h1">{content.name}</Heading>
+                )}
+                {content.image && (
+                    <Image
+                        fit={'cover'}
+                        align={'center'}
+                        width={'100%'}
+                        height={'100%'}
+                        src={imageurl}
+                        alt={content.name}
+                    />
+                )}
+                {content.description && (
+                    <Text color={'gray.700'} fontWeight={600}>
+                        {content.description}
+                    </Text>
+                )}
+                {content.deliveryKey && (
+                    <Box paddingTop="2" width={{ base: 'full', md: 'auto' }}>
+                        <Button label="View" url={linkurl} target='_self'></Button>
+                    </Box>
+                )}
+
+
             </Box>
-             )}
-            </VStack>
-          </Box>
-          </Container>
         );
-      }
+    }
 
-    // TODO: render author, image
-
-    return  (
+    return (
         <Box data-testid="amplience-page" layerStyle="page">
-            <InstantSearch searchClient={searchClient} indexName="sfcccompdev.blog-staging">
-            
-                <SearchBox />
-                <RefinementList attribute="tags" />
-                <RefinementList attribute="categories" />
-                <RefinementList attribute="author.name" />
-                <SimpleGrid
-                    columns={{base: 1, md: 1, lg: 3}}
-                    spacingX={{base: 1, md: 4}}
-                    spacingY={{base: 4, md: 14}}
-                >
-                    <Hits hitComponent={Hit} />
+            <VStack>
+                <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                        <SearchIcon />
+                    </InputLeftElement>
+                    <Input
+                        autoComplete="off"
+                        id="search-input"
+                        onChange={(e) => onSearchInputChange(e)}
+                        type="search"
+                        variant="filled"
+                    />
+                </InputGroup>
+                <SimpleGrid columns={[1, 1, 3]} spacing='40px'>
+                    <Select placeholder='Select Tags' onChange={(e) => onTagDropDownChanged(e)}>
+                        {
+                            tags.map((item, index) => {
+                                var text = item.value + ` (${item.count})`
+                                return <option value={item.value}>{text}</option>
+                            })
+                        }
+                    </Select>
+                    <Select placeholder='Select Categories' onChange={(e) => onCategoryDropDownChanged(e)}>
+                        {
+                            categories.map((item, index) => {
+                                var text = item.value + ` (${item.count})`
+                                return <option value={item.value}>{text}</option>
+                            })
+                        }
+                    </Select>
+                    <Select placeholder='Select Authors' onChange={(e) => onAuthorDropDownChanged(e)}>
+                        {
+                            authors.map((item, index) => {
+                                var text = item.value + ` (${item.count})`
+                                return <option value={item.value}>{text}</option>
+                            })
+                        }
+                    </Select>
                 </SimpleGrid>
-            </InstantSearch>
+            </VStack>
+            <SimpleGrid
+                columns={[1, 2, 3, 4]}
+                spacingX={10}
+                spacingY={{ base: 12, lg: 16 }}
+            >
+                {
+                    results.map((item, index) => {
+                        return <BlogCard item={item} key={index} />
+                    })
+                }
+            </SimpleGrid>
         </Box>
     )
 }
