d4633bd9866883538031d2e60e357aacbde60ffc
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2022-09-01 17:57:42 +0100

    feat: Urls working and transforming category names

--- app/pages/amplience/blog-landing/index.jsx
+++ app/pages/amplience/blog-landing/index.jsx
@@ -6,10 +6,11 @@ import { useIntl } from 'react-intl'
 
 import amplience from '/config/amplience/default.js'
 import {AmplienceContext} from '../../../contexts/amplience'
+import {useCategories} from '../../../hooks/use-categories'
 
 // Components
 import Button from '../../../components/amplience/button'
-import { SearchIcon, ChevronLeftIcon, ChevronRightIcon } from '../../../components/icons'
+import { SearchIcon } from '../../../components/icons'
 import useLocale from '../../../hooks/use-locale'
 import {
     Box,
@@ -23,8 +24,7 @@ import {
     Text,
     Image,
     useMultiStyleConfig,
-    Select,
-    HStack
+    Select
 } from '@chakra-ui/react'
 
 
@@ -36,16 +36,11 @@ import {
  * categories and products, data is from local file.
  */
 const BlogLanding = (props) => {
-    const paginationstyles = useMultiStyleConfig('Pagination')
-    const intl = useIntl()
-
     const locale = useLocale()
     const siteLocale = locale.id.toLowerCase()
 
     const {vse} = useContext(AmplienceContext)
-
-    
-
+    const {categories} = useCategories()
 
     const [results, setResults] = useState([]);
     const [query, setQuery] = useState('');
@@ -53,7 +48,7 @@ const BlogLanding = (props) => {
     const [author, setAuthor] = useState('');
     const [tags, setTags] = useState([]);
     const [tag, setTag] = useState('');
-    const [categories, setCategories] = useState([]);
+    const [thecategories, setCategories] = useState([]);
     const [category, setCategory] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
     const [maxIndex, setMaxIndex] = useState(0);
@@ -63,23 +58,22 @@ const BlogLanding = (props) => {
     const algoliaID = '4BS5I6EVVD'
     const algoliaKey = 'fa9be8756d95a97f0021f0aba6f02bc4'
     let indexType = vse ? 'staging' : 'production';
+    // TODO: Put back once data is there
     // const algoliaIndexName = amplience.hub+'.blog-'+indexType+'-'+siteLocale;
     const algoliaIndexName = 'sfcccompdev.blog-staging'
     const numItems = 8;
 
-    /*
-    const url = new URL(window.location);
-    url.searchParams.set('newParam', 'value');
-    window.history.pushState(null, '', url.toString());
-    */
-
     const searchClient = algoliasearch(algoliaID, algoliaKey);
     const index = searchClient.initIndex(algoliaIndexName);
 
-    const facetHitsToArray = (list) => {
-        let facetArray = []
+    const facetHitsToArray = (list, isCategory) => {
+        let facetArray = []        
         for (const property in list) {
-            facetArray.push({ value: property, count: list[property] })
+            let myLabel = property;
+            if(isCategory){
+                myLabel = categories[property]?.name || property
+            }
+            facetArray.push({ value: property, label: myLabel, count: list[property] })
         }
         return facetArray;
 
@@ -111,10 +105,10 @@ const BlogLanding = (props) => {
             filtersArray.push('author.name:' + author)
         }
         if (category) {
-            filtersArray.push('categories.name:' + category)
+            filtersArray.push('categories.id:' + category)
         }
         index.search(query, {
-            facets: ['tags', 'author.name', 'categories.name'],
+            facets: ['tags', 'author.name', 'categories.id'],
             facetFilters: filtersArray,
             hitsPerPage: numItems,
             page: currentIndex
@@ -128,7 +122,7 @@ const BlogLanding = (props) => {
             }
             setPages(pagesArray)
             setTags(facetHitsToArray(results.facets.tags))
-            setCategories(facetHitsToArray(results.facets['categories.name']))
+            setCategories(facetHitsToArray(results.facets['categories.id'], true))
             setAuthors(facetHitsToArray(results.facets['author.name']))
             setResults(results.hits)
 
@@ -147,7 +141,6 @@ const BlogLanding = (props) => {
     const onSearchInputChange = (e) => {
         const input = e.target.value;
         setUrlParams('query', input)
-        // Should set the query
         setQuery(input)
     }
 
@@ -193,7 +186,7 @@ const BlogLanding = (props) => {
                 {content.image && (
                     <Image
                         fit={'cover'}
-                        //align={'center'}
+                        align={'center'}
                         width={'100%'}
                         height={'233'}
                         src={imageurl}
@@ -240,15 +233,15 @@ const BlogLanding = (props) => {
                     <Select placeholder='Select Tags' onChange={(e) => onTagDropDownChanged(e)}>
                         {
                             tags.map((item, index) => {
-                                var text = item.value + ` (${item.count})`
+                                var text = item.label + ` (${item.count})`
                                 return <option value={item.value}>{text}</option>
                             })
                         }
                     </Select>
                     <Select placeholder='Select Categories' onChange={(e) => onCategoryDropDownChanged(e)}>
                         {
-                            categories.map((item, index) => {
-                                var text = item.value + ` (${item.count})`
+                            thecategories.map((item, index) => {
+                                var text = item.label + ` (${item.count})`
                                 return <option value={item.value}>{text}</option>
                             })
                         }
@@ -256,7 +249,7 @@ const BlogLanding = (props) => {
                     <Select placeholder='Select Authors' onChange={(e) => onAuthorDropDownChanged(e)}>
                         {
                             authors.map((item, index) => {
-                                var text = item.value + ` (${item.count})`
+                                var text = item.label + ` (${item.count})`
                                 return <option value={item.value}>{text}</option>
                             })
                         }
