68d8eb61b5a5d91db6eb4edf8e75b35fdc306dfb
rs-amp <rsimpson@amplience.com>
2022-09-13 14:32:42 +0100

    fix: fix page count with in-grid content

--- app/pages/product-list/index.jsx
+++ app/pages/product-list/index.jsx
@@ -134,6 +134,11 @@ const calculatePageOffsets = (pageSize, totalCount, ampSlots, isMobile) => {
         for (let i = 0; i < ampSlots.length; i++) {
             const slot = ampSlots[i]
 
+            if (slot.position > totalCount + offset) {
+                // Slots outside of the bounds of the shown products are not drawn.
+                break
+            }
+
             fillPages(slot.position)
 
             const size = isMobile ? 1 : Number(slot.cols) * Number(slot.rows)
@@ -142,7 +147,7 @@ const calculatePageOffsets = (pageSize, totalCount, ampSlots, isMobile) => {
         }
     }
 
-    fillPages(totalCount)
+    fillPages(totalCount + offset)
 
     return pages
 }
