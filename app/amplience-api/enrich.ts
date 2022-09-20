1bc959e7df6dd036963703a6bdcf214e2cb4c4ad
rs-amp <rsimpson@amplience.com>
2022-09-20 17:12:25 +0100

    feat: add generalized enrich method for fetch content

---
+++ app/amplience-api/enrich.ts
@@ -0,0 +1,79 @@
+const referenceTypes = [
+    'http://bigcontent.io/cms/schema/v1/core#/definitions/content-link',
+    'http://bigcontent.io/cms/schema/v1/core#/definitions/content-reference'
+]
+
+export interface EnrichTarget {
+    index: string | number;
+
+    item: any;
+    parent: any;
+}
+
+export interface EnrichStrategy {
+    trigger: (content: any) => boolean;
+    enrich: (content: EnrichTarget[]) => Promise<void>;
+}
+
+export const isPersonalized = (item: any): boolean => {
+    return Object.keys(item).length === 2 && Array.isArray(item.segment) && Array.isArray(item)
+}
+
+export const isContentReference = (item: any): boolean => {
+    return (
+        item._meta &&
+        referenceTypes.indexOf(item._meta.schema) !== -1 &&
+        typeof item.contentType === 'string' &&
+        typeof item.id === 'string'
+    )
+}
+
+export const getEnrichTargets = (
+    item: any,
+    enrichStrategies: EnrichStrategy[],
+    enrichTargets: EnrichTarget[][],
+    parent?: any,
+    index?: string | number
+) => {
+    if (Array.isArray(item)) {
+        item.forEach((contained, i) => {
+            getEnrichTargets(contained, enrichStrategies, enrichTargets, item, i)
+        })
+    } else if (item != null && typeof item === 'object') {
+        const allPropertyNames = Object.getOwnPropertyNames(item)
+        // Does this object match the pattern expected for enrich?
+
+        for (let i = 0; i < enrichStrategies.length; i++) {
+            if (enrichStrategies[i].trigger(item) && index != null) {
+                enrichTargets[i].push({
+                    parent,
+                    index,
+                    item
+                })
+            }
+        }
+
+        allPropertyNames.forEach((propName) => {
+            const prop = item[propName]
+            if (typeof prop === 'object') {
+                getEnrichTargets(prop, enrichStrategies, enrichTargets, item, propName)
+            }
+        })
+    }
+}
+
+export const enrichContent = async (item: any, enrichStrategies: EnrichStrategy[]) => {
+    const targets: EnrichTarget[][] = []
+    for (let i = 0; i < enrichStrategies.length; i++) {
+        targets.push([])
+    }
+
+    await getEnrichTargets(item, enrichStrategies, targets)
+
+    for (let i = 0; i < enrichStrategies.length; i++) {
+        const group = targets[i]
+        if (group.length > 0) {
+            enrichStrategies[i].enrich(group)
+        }
+    }
+}
--- app/amplience-api/index.ts
+++ app/amplience-api/index.ts
@@ -1,6 +1,7 @@
 import {ContentClient, ContentItem, ContentReference} from 'dc-delivery-sdk-js'
 import {chunk, flatten} from 'lodash'
 import {app} from '../../config/default'
+import {EnrichTarget, isContentReference, enrichContent, isPersonalized} from './enrich'
 
 export type IdOrKey = {id: string} | {key: string}
 export type FilterType = ((item: any) => boolean) | undefined
@@ -22,11 +23,6 @@ export type FetchParams = {
     format?: 'inlined';
     client?: ContentClient;
 }
-
-const referenceTypes = [
-    'http://bigcontent.io/cms/schema/v1/core#/definitions/content-link',
-    'http://bigcontent.io/cms/schema/v1/core#/definitions/content-reference'
-]
 
 const isTimeMachineVse = (vse: string): boolean => {
     if (vse == null || vse.length === 0) {
@@ -94,7 +90,9 @@ export class AmplienceAPI {
         delete params.client
         const chunks = chunk(args, 12)
 
-        let responses = await Promise.all(chunks.map(async (arg) => (await client.getContentItems(arg, params)).responses))
+        let responses = await Promise.all(
+            chunks.map(async (arg) => (await client.getContentItems(arg, params)).responses)
+        )
 
         return flatten(responses).map((response) => {
             if ('content' in response) {
@@ -127,33 +125,29 @@ export class AmplienceAPI {
         await Promise.all(items.map((item) => this.getChildren(item, filter)))
     }
 
-    getReferences(item: any, refs: Map<string, any>) {
-        if (Array.isArray(item)) {
-            item.forEach((contained) => {
-                this.getReferences(contained, refs)
-            })
-        } else if (item != null && typeof item === 'object') {
-            const allPropertyNames = Object.getOwnPropertyNames(item)
-            // Does this object match the pattern expected for a content item or reference?
-            if (
-                item._meta &&
-                referenceTypes.indexOf(item._meta.schema) !== -1 &&
-                typeof item.contentType === 'string' &&
-                typeof item.id === 'string'
-            ) {
-                if (!refs.get(item.id)) {
-                    refs.set(item.id, [])
-                }
-                refs.set(item.id, [...refs.get(item.id), item])
-                return
-            }
+    async enrichReferenceDeliveryKeysInternal(targets: EnrichTarget[], locale: string) {
+        const ids = new Set<string>(targets.map((target) => target.item.id))
 
-            allPropertyNames.forEach((propName) => {
-                const prop = item[propName]
-                if (typeof prop === 'object') {
-                    this.getReferences(prop, refs)
+        if (ids.size > 0) {
+            const items = await this.fetchContent(
+                Array.from(ids).map((id) => ({
+                    id
+                })),
+                {
+                    locale,
+                    depth: 'root',
+                    client: this.hierarchyClient
                 }
-            })
+            )
+
+            for (let item of items) {
+                const key = item._meta.deliveryKey
+                if (key) {
+                    targets
+                        .filter((target) => target.item.id === item._meta.deliveryId)
+                        .forEach((target) => (target.item.deliveryKey = key))
+                }
+            }
         }
     }
 
@@ -184,30 +178,30 @@ export class AmplienceAPI {
         }
     }
 
-    async enrichReferenceDeliveryKeys(item: any, locale = 'en-US') {
-        const refs = new Map<string, any>()
-
-        this.getReferences(item, refs)
-
-        const ids = Array.from(refs.keys()).map((id) => ({id}))
-
-        if (ids.length > 0) {
-            const items = await this.fetchContent(ids, {
-                locale,
-                depth: 'root',
-                client: this.hierarchyClient
-            })
-
-            for (let item of items) {
-                const key = item._meta.deliveryKey
-                if (key) {
-                    refs.get(item._meta.deliveryId).map((el) => {
-                        el.deliveryKey = key
-                        return el
-                    })
-                }
-            }
+    async enrichVariantsInternal(targets: EnrichTarget[], locale: string) {
+        for (let target of targets) {
+            const item = target.item
+            target.parent[target.index] = await this.getVariantsContent({variants: item}, {locale})
         }
+    }
+
+    async enrichVariants(item: any, locale = 'en-US') {
+        await enrichContent(item, [
+            {
+                trigger: isPersonalized,
+                enrich: (targets: EnrichTarget[]) => this.enrichVariantsInternal(targets, locale)
+            }
+        ])
+    }
+
+    async enrichReferenceDeliveryKeys(item: any, locale = 'en-US') {
+        await enrichContent(item, [
+            {
+                trigger: isContentReference,
+                enrich: (targets: EnrichTarget[]) =>
+                    this.enrichReferenceDeliveryKeysInternal(targets, locale)
+            }
+        ])
     }
 
     async fetchHierarchy(parent: IdOrKey, filter: FilterType, locale = 'en-US') {
