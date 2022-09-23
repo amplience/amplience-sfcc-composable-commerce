b05f8fcf4f811096c5be05da185c480ea2105833
rs-amp <rsimpson@amplience.com>
2022-09-23 15:26:14 +0100

    fix: fix some validation errors

--- app/components/amplience/personalised-component/index.jsx
+++ app/components/amplience/personalised-component/index.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import AmplienceWrapper from '../wrapper'
 
-const PersonalisedComponent = ({content, components, limit, ...props}) => {
+const PersonalisedComponent = ({
+    content,
+    components,
+    limit,
+    defaultContent,
+    maxNumberMatches,
+    variants,
+    ...props
+}) => {
     if (content) {
         if (limit) {
             content = content.slice(0, limit)
@@ -26,7 +34,11 @@ PersonalisedComponent.displayName = 'PersonalisedComponent'
 PersonalisedComponent.propTypes = {
     content: PropTypes.array,
     components: PropTypes.object,
-    limit: PropTypes.number
+    limit: PropTypes.number,
+    // These are not used, but content does contain it.
+    defaultContent: PropTypes.object,
+    maxNumberMatches: PropTypes.number,
+    variants: PropTypes.array
 }
 
 export default PersonalisedComponent
