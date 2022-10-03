fc77c615f4d4d62db93a13647732d63ddb76f6a9
Réza Kalfane <reza.kalfane@gmail.com>
2022-10-03 21:05:44 +0200

    feat: added current env and envs list to contetx

--- app/contexts/amplience/index.js
+++ app/contexts/amplience/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import * as cookie from 'cookie'
 import {AmplienceAPI, defaultAmpClient} from '../../amplience-api'
+import { default as amplience } from '../../../config/amplience/default.js'
 
 /**
  * This is the global Amplience Realtime Visualization Context on Non-category pages
@@ -71,6 +72,8 @@ export const AmplienceContextProvider = ({vse, vseTimestamp, groups: initialGrou
     // Init client using VSE
     const [client] = useState(new AmplienceAPI())
     const [groups, setGroups] = useState(initialGroups)
+    const [envs, setEnvs] = useState(amplience.envs)
+    const [currentEnv, setCurrentEnv] = useState(amplience.default)
 
     useEffect(() => {
         if (groups !== initialGroups) {
@@ -81,6 +84,10 @@ export const AmplienceContextProvider = ({vse, vseTimestamp, groups: initialGrou
     // Switch the API to use the provided VSE, if present.
     client.setGroups(groups)
     client.setVse(vse)
+    
+    const setNewCurrentEnv = (env) => {
+        setCurrentEnv(env)
+    }
 
     const updateGroups = (groups) => {
         defaultAmpClient.setGroups(groups)
@@ -89,7 +96,18 @@ export const AmplienceContextProvider = ({vse, vseTimestamp, groups: initialGrou
     }
 
     return (
-        <AmplienceContext.Provider value={{vse, vseTimestamp, groups, updateGroups, client}}>
+        <AmplienceContext.Provider value={
+            {
+                vse, 
+                vseTimestamp, 
+                envs, 
+                currentEnv, 
+                groups, 
+                setNewCurrentEnv, 
+                updateGroups, 
+                client
+                }
+        }>
             {children}
         </AmplienceContext.Provider>
     )
@@ -154,6 +172,7 @@ AmplienceContextProvider.propTypes = {
     vse: PropTypes.string,
     vseTimestamp: PropTypes.number,
     groups: PropTypes.array,
-
+    currentEnv: PropTypes.any,
+    envs: PropTypes.array,
     children: PropTypes.node.isRequired
 }
