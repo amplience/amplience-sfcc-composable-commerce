7fef0f59522bec992442d6d331d73ef91d52cb9d
Réza Kalfane <reza.kalfane@gmail.com>
2022-10-03 16:23:03 +0200

    feat: enriched config

--- config/amplience/default.js
+++ config/amplience/default.js
@@ -1,27 +1,32 @@
 module.exports = {
-    defaultEnvName: 'Live',
+    name: 'Live',
     hub: 'sfcccomposable',
     vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io',
+    url: 'https://ascc-production.mobify-storefront.com',
     envs: [
         {
             name: 'Live',
             hub: 'sfcccomposable',
-            vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io'
+            vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io',
+            url: 'https://ascc-production.mobify-storefront.com'
         },
         {
             name: 'UAT',
             hub: 'sfcccompuat',
-            vse: 'e7ssecddcjvq1470dg00ennz9.staging.bigcontent.io'
+            vse: 'e7ssecddcjvq1470dg00ennz9.staging.bigcontent.io',
+            url: 'https://ascc-uat.mobify-storefront.com'
         },
         {
             name: 'Development 2',
             hub: 'sfcccompdev2',
-            vse: '1jade04tmvsq21rjusvaue8p27.staging.bigcontent.io'
+            vse: '1jade04tmvsq21rjusvaue8p27.staging.bigcontent.io',
+            url: 'https://ascc-development-2.mobify-storefront.com'
         },
         {
             name: 'Development',
             hub: 'sfcccompdev',
-            vse: '11pas6cl3x9ck1bbgcbsl1e6tk.staging.bigcontent.io'
+            vse: '11pas6cl3x9ck1bbgcbsl1e6tk.staging.bigcontent.io',
+            url: 'https://ascc-development.mobify-storefront.com'
         }
     ]
 }
