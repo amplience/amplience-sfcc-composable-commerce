66f322a79a6aea8e786aa57583c4b631b57da14d
Réza Kalfane <reza.kalfane@gmail.com>
2022-10-03 15:48:32 +0200

    feat: added new envirnoments

--- config/amplience/default.js
+++ config/amplience/default.js
@@ -1,3 +1,27 @@
 module.exports = {
-    hub: 'sfcccomposable'
+    defaultEnvName: 'Live',
+    hub: 'sfcccomposable',
+    vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io',
+    envs: [
+        {
+            name: 'Live',
+            hub: 'sfcccomposable',
+            vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io'
+        },
+        {
+            name: 'UAT',
+            hub: 'sfcccompuat',
+            vse: 'e7ssecddcjvq1470dg00ennz9.staging.bigcontent.io'
+        },
+        {
+            name: 'Development 2',
+            hub: 'sfcccompdev2',
+            vse: '1jade04tmvsq21rjusvaue8p27.staging.bigcontent.io'
+        },
+        {
+            name: 'Development',
+            hub: 'sfcccompdev',
+            vse: '11pas6cl3x9ck1bbgcbsl1e6tk.staging.bigcontent.io'
+        }
+    ]
 }
