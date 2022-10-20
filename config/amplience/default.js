a8b30377a791881788277aa23f8d50c969c091e8
Réza Kalfane <reza.kalfane@gmail.com>
2022-10-20 10:36:33 +0200

    chore: only sfcccomposable in config

--- config/amplience/default.js
+++ config/amplience/default.js
@@ -1,52 +1,8 @@
 module.exports = {
     default: {
-        name: 'Development 2',
-        hub: 'sfcccompdev2',
-        vse: '1jade04tmvsq21rjusvaue8p27.staging.bigcontent.io',
-        url: 'https://ascc-development-2.mobify-storefront.com'
-    },
-    envs: [
-        {
-            name: 'Live',
-            hub: 'sfcccomposable',
-            vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io',
-            url: 'https://ascc-production.mobify-storefront.com'
-        },
-        {
-            name: 'UAT',
-            hub: 'sfcccompuat',
-            vse: 'e7ssecddcjvq1470dg00ennz9.staging.bigcontent.io',
-            url: 'https://ascc-uat.mobify-storefront.com'
-        },
-        {
-            name: 'Development 2',
-            hub: 'sfcccompdev2',
-            vse: '1jade04tmvsq21rjusvaue8p27.staging.bigcontent.io',
-            url: 'https://ascc-development-2.mobify-storefront.com'
-        },
-        {
-            name: 'Development',
-            hub: 'sfcccompdev',
-            vse: '11pas6cl3x9ck1bbgcbsl1e6tk.staging.bigcontent.io',
-            url: 'https://ascc-development.mobify-storefront.com'
-        },
-        {
-            name: 'Automation Test 01 - Réza',
-            hub: 'automation01',
-            vse: '1dpf8i5xxl3cn1xccm0grjyowp.staging.bigcontent.io',
-            url: ''
-        },
-        {
-            name: 'Automation Test 02 - Réza',
-            hub: 'automation02',
-            vse: 'jf7r926acbou18fx7sz7374ij.staging.bigcontent.io',
-            url: ''
-        },
-        {
-            name: 'Automation Test 03 - Réza',
-            hub: 'automation03',
-            vse: 'uydimqk8ysu21nh1pqhm0w8ry.staging.bigcontent.io',
-            url: ''
-        }
-    ]
+        name: 'Live',
+        hub: 'sfcccomposable',
+        vse: '1pqebtas9axy7zeeiy96389op.staging.bigcontent.io',
+        url: 'https://ascc-production.mobify-storefront.com'
+    }
 }
