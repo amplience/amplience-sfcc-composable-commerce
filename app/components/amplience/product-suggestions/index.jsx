ee313faf8c195bcc8a6469e33c45e6dffa632bf9
Réza Kalfane <reza.kalfane@gmail.com>
2022-10-25 16:14:25 +0200

    fix: issue with box size

--- app/components/amplience/product-suggestions/index.jsx
+++ app/components/amplience/product-suggestions/index.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import {Text, Stack, Box, HStack, VStack, Skeleton, AspectRatio, SimpleGrid, useBreakpointValue, SkeletonText, Image} from '@chakra-ui/react'
-import {useCommerceAPI} from '../../../commerce-api/contexts'
-import {handleAsyncError} from '../../../commerce-api/utils'
+import { Text, Stack, Box, HStack, VStack, Skeleton, AspectRatio, SimpleGrid, useBreakpointValue, SkeletonText, Image } from '@chakra-ui/react'
+import { useCommerceAPI } from '../../../commerce-api/contexts'
+import { handleAsyncError } from '../../../commerce-api/utils'
 import { productUrlBuilder } from '../../../utils/url'
 import { useCurrency } from '../../../hooks'
-import {useIntl} from 'react-intl'
+import { useIntl } from 'react-intl'
 
 const selectImage = (product) => {
     const groups = product.imageGroups
@@ -19,15 +19,15 @@ const selectImage = (product) => {
     return desiredGroup.images[0]
 }
 
-const ProductSuggestions = ({suggestions, closeAndNavigate}) => {
+const ProductSuggestions = ({ suggestions, closeAndNavigate }) => {
     if (!suggestions) {
         return null
     }
     const api = useCommerceAPI()
     const [apiProducts, setApiProducts] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const isMobile = useBreakpointValue({base: true, lg: false, xl: false, xxl: false, xxxl: false})
-    const {currency: activeCurrency} = useCurrency()
+    const isMobile = useBreakpointValue({ base: true, lg: false, xl: false, xxl: false, xxxl: false })
+    const { currency: activeCurrency } = useCurrency()
     const intl = useIntl()
 
     useEffect(() => {
@@ -43,14 +43,14 @@ const ProductSuggestions = ({suggestions, closeAndNavigate}) => {
         handleAsyncError(async () => {
             const products = suggestions.map(suggestion => suggestion.productId)
             const response = await api.shopperProducts.getProducts({
-                parameters: {ids: products.join(',')}
+                parameters: { ids: products.join(',') }
             })
 
             if (active) {
                 const products = response.data.map((product) => ({
                     ...product,
                     productId: product.id,
-                    link: productUrlBuilder({id: product.id}),
+                    link: productUrlBuilder({ id: product.id }),
                     image: selectImage(product)
                 }))
 
@@ -64,74 +64,75 @@ const ProductSuggestions = ({suggestions, closeAndNavigate}) => {
 
     return (
         <>
-        <SimpleGrid columns={{sm: 1, lg: 3}} spacing={2}>
-            {
-                isLoading && !isMobile &&
-                    [...new Array(6)].map(item => 
+            <SimpleGrid columns={{ sm: 1, lg: 3 }} spacing={2}>
+                {
+                    isLoading && !isMobile &&
+                    [...new Array(6)].map(item =>
                         <Stack>
                             <AspectRatio ratio={1}>
                                 <Skeleton />
                             </AspectRatio>
-                            <SkeletonText noOfLines={2}/>
+                            <SkeletonText noOfLines={2} />
                         </Stack>
                     )
-            }
-            {
-                isLoading && isMobile &&
-                <Box w={'100%'} alignItems={'flex-start'}>
-                    {
-                        [...new Array(6)].map(item => 
-                            <HStack h={'48px'} gap={4}>
-                                <Skeleton w={'40px'} h={'40px'} />
-                                <SkeletonText w={'100px'} noOfLines={1}/>
-                                <SkeletonText w={'50px'} noOfLines={1}/>
-                            </HStack>
-                        )
-                    }
-                </Box>
-            }
-            {
-                apiProducts && 
-                apiProducts.map(item=>
+                }
+                {
+                    isLoading && isMobile &&
                     <Box w={'100%'} alignItems={'flex-start'}>
                         {
-                            !isMobile &&
-                            <VStack
-                                style={{cursor: 'pointer'}}
-                                onMouseDown={() => closeAndNavigate(item.link)}
-                            >
-                                
-                                <AspectRatio ratio={1}>
-                                    <Box 
-                                            w={'140px'}
-                                            style={{minWidth: '140px'}}
-                                        >
-                                        <Image
-                                            src={item.image.link}
-                                            width={140}
-                                            alt={item.image.alt}
-                                        />
-                                    </Box>
-                                </AspectRatio>
-                                <Text fontSize={'xs'} fontWeight={700}>{item.name}</Text>
-                                <Text fontSize={'xs'}>
-                                {intl.formatNumber(item.price, {
-                                    style: 'currency',
-                                    currency: item.currency || activeCurrency
-                                })}
-                                </Text>
-                            </VStack>
+                            [...new Array(6)].map(item =>
+                                <HStack h={'48px'} gap={4}>
+                                    <Skeleton w={'40px'} h={'40px'} />
+                                    <SkeletonText w={'100px'} noOfLines={1} />
+                                    <SkeletonText w={'50px'} noOfLines={1} />
+                                </HStack>
+                            )
                         }
-                        {
-                            isMobile &&
-                                <HStack h={'40px'}
-                                    gap={4}
-                                    style={{cursor: 'pointer'}}
+                    </Box>
+                }
+                {
+                    apiProducts &&
+                    apiProducts.map(item =>
+                        <Box w={'100%'} alignItems={'flex-start'}>
+                            {
+                                !isMobile &&
+                                <VStack
+                                    style={{ cursor: 'pointer' }}
                                     onMouseDown={() => closeAndNavigate(item.link)}
                                 >
-                                    <Box 
+
+                                    <Box
+                                        w={'140px'}
+                                        h={'140px'}
+                                        style={{ minWidth: '140px' }}
+                                    >
+                                        <AspectRatio ratio={1}>
+                                            <Image
+                                                src={item.image.link}
+                                                width={140}
+                                                alt={item.image.alt}
+                                            />
+                                        </AspectRatio>
+                                    </Box>
+                                    <Text fontSize={'xs'} fontWeight={700}>{item.name}</Text>
+                                    <Text fontSize={'xs'}>
+                                        {intl.formatNumber(item.price, {
+                                            style: 'currency',
+                                            currency: item.currency || activeCurrency
+                                        })}
+                                    </Text>
+                                </VStack>
+                            }
+                            {
+                                isMobile &&
+                                <HStack h={'40px'}
+                                    gap={4}
+                                    style={{ cursor: 'pointer' }}
+                                    onMouseDown={() => closeAndNavigate(item.link)}
+                                >
+                                    <Box
                                         w={'40px'}
-                                        style={{minWidth: '40px'}}
+                                        style={{ minWidth: '40px' }}
                                     >
                                         <Image
                                             src={item.image.link}
@@ -141,16 +142,16 @@ const ProductSuggestions = ({suggestions, closeAndNavigate}) => {
                                     </Box>
                                     <Text fontSize='xs' fontWeight={700}>{item.name}</Text>
                                     <Text fontSize='xs'>
-                                    {intl.formatNumber(item.price, {
-                                        style: 'currency',
-                                        currency: item.currency || activeCurrency
-                                    })}
+                                        {intl.formatNumber(item.price, {
+                                            style: 'currency',
+                                            currency: item.currency || activeCurrency
+                                        })}
                                     </Text>
                                 </HStack>
-                    }
-                    </Box>)
-            }
-        </SimpleGrid>
+                            }
+                        </Box>)
+                }
+            </SimpleGrid>
         </>
     )
 }
