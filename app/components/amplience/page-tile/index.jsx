e3ae7e159bdcf7ee93dc711010d9d0c9a84a5d40
rs-amp <rsimpson@amplience.com>
2022-11-01 14:14:06 +0000

    fix page search links not passing vse params or locale

--- app/components/amplience/page-tile/index.jsx
+++ app/components/amplience/page-tile/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Box, Link, Text} from '@chakra-ui/react'
+import {Box, Text} from '@chakra-ui/react'
+import Link from '../link'
 
 const PageTile = ({page}) => {
     return (
         <Box>
-            <Link href={'/' + page._meta.deliveryKey}>
+            <Link to={'/' + page._meta.deliveryKey}>
                 <Text fontSize="lg">{page.seo.title}</Text>
             </Link>
             <Text noOfLines={2}>{page.seo.description}</Text>
