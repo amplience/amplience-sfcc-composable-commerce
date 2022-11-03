5a62ba8d1905d2079d210010d600bdde2d6ed105
Ray Sweeten <ray@raysweeten.com>
2022-11-03 14:20:22 -0400

    fix: CSS in rich-text component, not static file

--- app/components/amplience/rich-text/index.jsx
+++ app/components/amplience/rich-text/index.jsx
@@ -3,8 +3,87 @@ import PropTypes from 'prop-types'
 import AmplienceWrapper from '../wrapper'
 import AmplienceMarkdown from '../markdown'
 import {Box, Heading} from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
 import {TrueAdaptiveImage} from '../adaptive-image'
+
+const Contain = styled(Box)`
+    .amp-rich-text h1 {
+        font-size: 3rem;
+        font-weight: 800;
+        margin-bottom: 0.4em;
+    }
+
+    .amp-rich-text h2 {
+        font-size: 2.25rem;
+        font-weight: 700;
+        margin-bottom: 0.4em;
+    }
+
+    .amp-rich-text h3 {
+        font-size: 1.875rem;
+        font-weight: 700;
+        margin-bottom: 0.4em;
+    }
+
+    .amp-rich-text h4 {
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin-bottom: 0.4em;
+    }
+
+    .amp-rich-text h5 {
+        font-size: 1.25rem;
+        font-weight: 700;
+        margin-bottom: 0.4em;
+    }
+
+    .amp-rich-text h6 {
+        font-size: 0.8rem;
+        font-weight: 700;
+        margin-bottom: 0.6em;
+    }
+
+    .amp-rich-text > p {
+        margin-block-end: 1em;
+    }
+
+    .amp-rich-text > p > img {
+        margin: 0 auto;
+    }
+
+    .amp-rich-text img {
+        max-height: 50vh;
+    }
+
+    .amp-rich-text a {
+        color: #0176d3;
+        font-weight: 600;
+    }
+
+    .amp-rich-text blockquote {
+        border-left: 5px solid #c9c9c9;
+        padding-inline-start: 1em;
+        padding-top: 0.25em;
+        padding-bottom: 0.25em;
+        margin-inline-start: 1em;
+        margin-block-end: 1em;
+    }
+
+    .amp-rich-text pre {
+        margin-block-end: 1em;
+    }
+
+    .amp-rich-text ul {
+        margin-inline-start: 1.5em;
+        margin-block-end: 1em;
+    }
+
+    .amp-rich-text ol {
+        margin-inline-start: 1.5em;
+        margin-block-end: 1em;
+    }
+`
 
 /**
  * Amplience Rich Text Component
@@ -14,7 +93,7 @@ import {TrueAdaptiveImage} from '../adaptive-image'
 const AmplienceRichText = ({header, content}) => {
     return (
         <>
-            <Box>
+            <Contain>
                 {header && (
                     <Heading pb="8" as="h2">
                         {header}
@@ -56,7 +135,7 @@ const AmplienceRichText = ({header, content}) => {
                         }
                     }
                 })}
-            </Box>
+            </Contain>
         </>
     )
 }
