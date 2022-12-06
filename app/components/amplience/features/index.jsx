c8bf5d742f58535b33362823fbdef7b674a8b8cc
rs-amp <rsimpson@amplience.com>
2022-12-06 16:21:43 +0000

    feat: add different features renders

--- app/components/amplience/features/index.jsx
+++ app/components/amplience/features/index.jsx
@@ -1,17 +1,75 @@
 import React from 'react'
 import {Text, Image, useTheme} from '@chakra-ui/react'
 import PropTypes from 'prop-types'
-import {Container, SimpleGrid, VStack, HStack, Flex} from '@chakra-ui/layout'
+import {Box, Container, SimpleGrid, VStack, HStack, Flex} from '@chakra-ui/layout'
 import {getImageUrl} from '../../../utils/amplience/image'
 import Link from '../link'
 import {getLinkUrlEnum} from '../../../utils/amplience/link'
+import Section from '../../section'
 
-const Features = ({features}) => {
+const Features = ({features, render}) => {
     const theme = useTheme()
 
     const {iconBaseStyle} = theme.components.Icon
 
-    return (
+    const buttonMode = render == 'Buttons'
+
+    return buttonMode ? (
+        <Section
+            background={'gray.50'}
+            marginX="auto"
+            paddingY={{base: 8, md: 16}}
+            paddingX={{base: 4, md: 8}}
+            borderRadius="base"
+            width={{base: '100vw', md: 'inherit'}}
+            position={{base: 'relative', md: 'inherit'}}
+            left={{base: '50%', md: 'inherit'}}
+            right={{base: '50%', md: 'inherit'}}
+            marginTop="8"
+            marginBottom="8"
+            marginLeft={{base: '-50vw', md: 'auto'}}
+            marginRight={{base: '-50vw', md: 'auto'}}
+        >
+            <SimpleGrid
+                columns={{base: 1, md: 1, lg: 3}}
+                spacingX={{base: 1, md: 4}}
+                spacingY={{base: 4, md: 14}}
+            >
+                {features.map((feature, index) => {
+                    const contents = (
+                        <HStack>
+                            <Flex paddingLeft={6} height={24} align={'center'} justify={'center'}>
+                                <Image
+                                    {...iconBaseStyle}
+                                    maxWidth="12"
+                                    maxHeight="12"
+                                    src={getImageUrl(feature.icon)}
+                                ></Image>
+                            </Flex>
+                            <Text fontWeight="700">{feature.title ?? feature.link.label}</Text>
+                        </HStack>
+                    )
+
+                    return (
+                        <Box
+                            key={index}
+                            background={'white'}
+                            boxShadow={'0px 2px 2px rgba(0, 0, 0, 0.1)'}
+                            borderRadius={'4px'}
+                        >
+                            {feature.link && feature.link.value ? (
+                                <Link target="_blank" to={getLinkUrlEnum(feature.link)}>
+                                    {contents}
+                                </Link>
+                            ) : (
+                                contents
+                            )}
+                        </Box>
+                    )
+                })}
+            </SimpleGrid>
+        </Section>
+    ) : (
         <Container maxW={'6xl'} marginTop={10}>
             <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={10}>
                 {features.map((feature, index) => {
@@ -51,6 +109,7 @@ const Features = ({features}) => {
 
 Features.displayName = 'Features'
 Features.propTypes = {
+    render: PropTypes.string,
     features: PropTypes.arrayOf(
         PropTypes.shape({
             title: PropTypes.string,
