2a499e2167f97d3e6c915280ca32d8bdc518c54b
Ray Sweeten <rsweeten@amplience.com>
2023-06-12 21:42:22 -0400

    grid lock component pulled in

--- app/components/amplience/wrapper/index.jsx
+++ app/components/amplience/wrapper/index.jsx
@@ -20,6 +20,7 @@ import CardList from '../card-list'
 import {useLocation} from 'react-router-dom'
 import ShoppableVideo from '../shoppable-video'
 import Features from '../features'
+import GridLock from '../grid-lock'
 
 const Blank = () => <></>
 
@@ -38,6 +39,7 @@ const componentsMapping = {
     'https://sfcc.com/slots/personalised-slot': flexibleListSlot,
     'https://sfcc.com/components/card-list': CardList,
     'https://sfcc.com/components/features': Features,
+    'https://sfcc.com/components/grid-lock': GridLock,
 
     'https://sfcc.com/site/navigation/root': Blank,
     'https://sfcc.com/site/navigation/external': Blank,
