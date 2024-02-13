a76ad89401d72e426d8112549ea5d24464bfcace
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2024-02-13 06:17:18 +0000

    chore: Linting errors and warnings for missing keys

--- app/components/amplience/personalised-experiences-list/index.jsx
+++ app/components/amplience/personalised-experiences-list/index.jsx
@@ -1,14 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
-import { AmplienceContext } from '../../../contexts/amplience'
-import { defaultAmpClient } from '../../../amplience-api'
-import { useIntl } from 'react-intl'
+import {AmplienceContext} from '../../../contexts/amplience'
+import {defaultAmpClient} from '../../../amplience-api'
+import {useIntl} from 'react-intl'
 import AmplienceWrapper from '../wrapper'
 
-const PersonalisedExperiencesList = ({
-    maxNumber,
-    categoryFilter = null
-}) => {
+const PersonalisedExperiencesList = ({maxNumber, categoryFilter = null, _meta}) => {
     const {groups} = useContext(AmplienceContext)
     const {locale} = useIntl()
     const [experiences, setExperiences] = useState([])
@@ -17,7 +14,11 @@ const PersonalisedExperiencesList = ({
         const retrieveAllExperiences = async () => {
             const newExperiences = []
             for (const group in groups) {
-                const experience = await defaultAmpClient.getPersonalisedExperiences(locale, groups[group], categoryFilter)
+                const experience = await defaultAmpClient.getPersonalisedExperiences(
+                    locale,
+                    groups[group],
+                    categoryFilter
+                )
                 newExperiences.push(...experience)
             }
             setExperiences(newExperiences)
@@ -25,28 +26,28 @@ const PersonalisedExperiencesList = ({
         retrieveAllExperiences()
     }, [groups, categoryFilter])
 
-    return <>
-        {
-            experiences 
-            && experiences.length
-            && experiences.slice(0, maxNumber).map((experience, index) => {
-                return <div>
-                    <AmplienceWrapper
-                        content={experience.content}
-                        key={index} 
-                    />
-                </div>
-            }) || <></>
-        }
-    </>
+    return (
+        <>
+            {(experiences &&
+                experiences.length &&
+                experiences.slice(0, maxNumber).map((experience, index) => {
+                    return (
+                        <div key={_meta?.deliveryId}>
+                            <AmplienceWrapper content={experience.content} key={index} />
+                        </div>
+                    )
+                })) || <></>}
+        </>
+    )
 }
 
 PersonalisedExperiencesList.displayName = 'PersonalisedExperiencesList'
 
 PersonalisedExperiencesList.propTypes = {
+    _meta: PropTypes.object,
     defaultContent: PropTypes.object,
     maxNumber: PropTypes.number,
-    categoryFilter: PropTypes.string,
+    categoryFilter: PropTypes.string
 }
 
 export default PersonalisedExperiencesList
